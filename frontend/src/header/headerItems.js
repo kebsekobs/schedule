a8a28ad50765fad905784032aebc1b8ce7e36a48
Mira <kruzinka@yandex.ru>
2024-06-11 21:04:09 +0300

    typo

--- frontend/src/header/headerItems.js
+++ frontend/src/header/headerItems.js
@@ -1,8 +1,7 @@
-export const headerItems =
-    [
-        {name:'Группы',path:'/groups'},
-        {name:'Аудитории',path:'/classrooms'},
-        {name:'Преподаватели',path:'/teachers'},
-        {name:'Дисциплины',path:'/disciplines'},
-        {name:'Сгеренировать'}
-    ]
+export const headerItems = [
+  { name: "Группы", path: "/groups" },
+  { name: "Аудитории", path: "/classrooms" },
+  { name: "Преподаватели", path: "/teachers" },
+  { name: "Дисциплины", path: "/disciplines" },
+  { name: "Сгенерировать" },
+];
