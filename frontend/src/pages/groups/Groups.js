6c326cdcb91d73151635565d806e9b48faa9b595
Mira <kruzinka@yandex.ru>
2024-06-12 14:17:37 +0300

    fix

--- frontend/src/pages/groups/Groups.js
+++ frontend/src/pages/groups/Groups.js
@@ -12,68 +12,6 @@ const Groups = () => {
     setIsAddModalOpen(!isAddModalOpen);
   };
 
-  // курсы мага
-  const firstYearMag = getGroupsQuery.data?.filter(
-    (group) =>
-      Math.floor(group.groupId / 100) === 1 && group.magistracy === true
-  );
-  const secondYearMag = getGroupsQuery.data?.filter(
-    (group) =>
-      Math.floor(group.groupId / 100) === 2 && group.magistracy === true
-  );
-
-  // курсы бакалавры
-  const firstYearBach = getGroupsQuery.data?.filter(
-    (group) =>
-      Math.floor(group.groupId / 100) === 1 && group.magistracy === false
-  );
-  const secondYearBach = getGroupsQuery.data?.filter(
-    (group) =>
-      Math.floor(group.groupId / 100) === 2 && group.magistracy === false
-  );
-  const thirdYearBach = getGroupsQuery.data?.filter(
-    (group) =>
-      Math.floor(group.groupId / 100) === 3 && group.magistracy === false
-  );
-  const fourthYearBach = getGroupsQuery.data?.filter(
-    (group) =>
-      Math.floor(group.groupId / 100) === 4 && group.magistracy === false
-  );
-  const fifthYearBach = getGroupsQuery.data?.filter(
-    (group) =>
-      Math.floor(group.groupId / 100) === 5 && group.magistracy === false
-  );
-
-  const data = [
-    {
-      year: '1 курс маг.',
-      subRows: firstYearMag,
-    },
-    {
-      year: '2 курс маг.',
-      subRows: secondYearMag,
-    },
-    {
-      year: '1 курс бак.',
-      subRows: firstYearBach,
-    },
-    {
-      year: '2 курс бак.',
-      subRows: secondYearBach,
-    },
-    {
-      year: '3 курс бак.',
-      subRows: thirdYearBach,
-    },
-    {
-      year: '4 курс бак.',
-      subRows: fourthYearBach,
-    },
-    {
-      year: '5 курс бак.',
-      subRows: fifthYearBach,
-    },
-  ]
 
   return (
     <>
@@ -83,7 +21,7 @@ const Groups = () => {
         ) : (
           <>
             <Button onClick={toggleAddModal}>Добавить группу</Button>
-            <CoursesTable data={data} />
+            <CoursesTable data={getGroupsQuery.data} />
             <AddGroupModal
               data={getGroupsQuery.data}
               isOpen={isAddModalOpen}
--- frontend/src/pages/groups/api/getGroupsQuery.js
+++ frontend/src/pages/groups/api/getGroupsQuery.js
@@ -5,7 +5,7 @@ export const useGroupsQuery = () => {
     return useQuery({
         queryKey: ['groups'],
         queryFn: () => service.getGroups(),
-        select: data => sortData(data)
+        select: data => sortToExpand(sortData(data))
     });
 };
 
@@ -17,3 +17,74 @@ function sortData(data) {
         return a.groupId - b.groupId;
     });
 }
+function sortToExpand (data) {
+// курсы мага
+const firstYearMag = data.filter(
+    (group) =>
+      Math.floor(group.groupId / 100) === 1 && group.magistracy === true
+  );
+  const secondYearMag = data.filter(
+    (group) =>
+      Math.floor(group.groupId / 100) === 2 && group.magistracy === true
+  );
+
+  // курсы бакалавры
+  const firstYearBach = data.filter(
+    (group) =>
+      Math.floor(group.groupId / 100) === 1 && group.magistracy === false
+  );
+  const secondYearBach = data.filter(
+    (group) =>
+      Math.floor(group.groupId / 100) === 2 && group.magistracy === false
+  );
+  const thirdYearBach = data.filter(
+    (group) =>
+      Math.floor(group.groupId / 100) === 3 && group.magistracy === false
+  );
+  const fourthYearBach = data.filter(
+    (group) =>
+      Math.floor(group.groupId / 100) === 4 && group.magistracy === false
+  );
+  const fifthYearBach = data.filter(
+    (group) =>
+      Math.floor(group.groupId / 100) === 5 && group.magistracy === false
+  );
+  const undefinedBanch = data.filter((group) => !group.groupId)
+  return [
+    {
+      year: '1 курс маг.',
+      subRows: firstYearMag,
+    },
+    {
+      year: '2 курс маг.',
+      subRows: secondYearMag,
+    },
+    {
+      year: '1 курс бак.',
+      subRows: firstYearBach,
+    },
+    {
+      year: '2 курс бак.',
+      subRows: secondYearBach,
+    },
+    {
+      year: '3 курс бак.',
+      subRows: thirdYearBach,
+    },
+    {
+      year: '4 курс бак.',
+      subRows: fourthYearBach,
+    },
+    {
+      year: '5 курс бак.',
+      subRows: fifthYearBach,
+    },
+    {
+        year: 'Неопрделенные',
+        subRows: undefinedBanch
+    }
+  ]
+}
+ 
+
+  
